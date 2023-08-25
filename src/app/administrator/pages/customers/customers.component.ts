48ead7f3fa517b646e177a0e4601b9679c70b4ec
Samuel Emeka <devsamuel0611@gmail.com>
2023-08-25 09:49:52 +0100

    customer tab

--- src/app/administrator/pages/customers/customers.component.ts
+++ src/app/administrator/pages/customers/customers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 
@@ -8,7 +8,7 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./customers.component.scss']
 })
 
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
   customerData = [
     {id: '1', firstname: 'Hawawu', lastname: 'Shittu', email: 'ishaqhawawu66@gmail.com', created_at: Date.now(), phone: '07066091112' , rank: 'Loyalty', order_count: 523, status: 'Active'},
     {id: '2', firstname: '-', lastname: '', email: 'tijanialiy@gmail.com', created_at: Date.now(), phone: '-' , rank: '-', order_count: '-', status: 'Inactive'},
@@ -16,6 +16,9 @@ export class CustomersComponent {
     {id: '4', firstname: 'Henry', lastname: 'Essien', email: 'henryvictor161@gmail.com', created_at: Date.now(), phone: '+2348060240322' , rank: 'Loyalty', order_count: 112, status: 'Active'},
     {id: '5', firstname: 'Benneth', lastname: 'Akintolure', email: 'bennettanderson8@gmail.com', created_at: Date.now(), phone: '07063012438' , rank: 'Loyalty', order_count: '-', status: 'Inactive'},
   ]
+  filter(opt) {
+    this.customerData.filter(opt)
+  }
   form = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
@@ -27,6 +30,14 @@ export class CustomersComponent {
   options = [
     {txt: 'Loyalty member', value: 'Loyalty member'}
   ];
+  selectedValue: string = 'All' //Default
+  filterOpts = [
+    {txt: 'All', value: "All"},
+    {txt: 'Active', value: "Active"},
+    {txt: 'Inactive', value: "Inactive"},
+ 
+  ]
+  filteredData = []
   f(n) {
     return this.form.get(n);
   }
@@ -35,4 +46,16 @@ export class CustomersComponent {
   }
   closeModal = new BehaviorSubject(false);
 
+  ngOnInit():void {
+    this.applyFilter();
+  }
+  applyFilter() {
+    if (this.selectedValue === 'All') {
+      this.filteredData = this.customerData;
+    }else {
+      this.filteredData = this.customerData.filter(item => {
+        return item.status === this.selectedValue
+      })
+    }
+  }
 }
