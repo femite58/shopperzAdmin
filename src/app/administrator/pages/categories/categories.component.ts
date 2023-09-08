206188bce9a537c380b040447837340bf8f71516
Samuel Emeka <devsamuel0611@gmail.com>
2023-09-08 22:43:57 +0100

    added categories and edited orders

--- src/app/administrator/pages/categories/categories.component.ts
+++ src/app/administrator/pages/categories/categories.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-categories',
@@ -8,20 +9,28 @@ import { FormControl } from '@angular/forms';
 })
 export class CategoriesComponent {
   
-  categories: [
+  closeModal = new BehaviorSubject(false);
+  submitting;
+
+  categories= [
     {
-      id: 1, name: 'Electronics',img_url:'assets/image_sm1.png', sub_cat: 'TV, Fridge, Iron, Phone', date_created: Date  
+      id: 1, name: 'Electronics',img_url:'assets/images/products/image_sm1.png', sub_cat: 'TV, Fridge, Iron, Phone', date_created: Date.now()
     },
     {
-      id: 2, name: 'Home accessories',img_url:'assets/image_sm2.png', sub_cat: '-', date_created: Date  
+      id: 2, name: 'Home accessories',img_url:'assets/images/products/image_sm2.png', sub_cat: '-', date_created: Date.now()  
     },
     {
-      id: 3, name: 'Gadgets',img_url:'assets/image_sm1.png', sub_cat: 'Bag, Keys, Glass', date_created: Date  
+      id: 3, name: 'Gadgets',img_url:'assets/images/products/image_sm1.png', sub_cat: 'Bag, Keys, Glass', date_created: Date.now()  
     },
     {
-      id: 4, name: 'Rado Watch',img_url:'assets/image_sm2.png', sub_cat: '', date_created: Date  
+      id: 4, name: 'Rado Watch',img_url:'assets/images/products/image_sm2.png', sub_cat: '', date_created: Date.now()  
     },
   ];
+  form = new FormGroup({
+    cat: new FormControl('', Validators.required),
+    img: new FormControl('', Validators.required),
+  });
 
-  
+  onClose() {}
+  submit() {}
 }
--- src/app/administrator/pages/orders/orders.component.ts
+++ src/app/administrator/pages/orders/orders.component.ts
@@ -83,15 +83,15 @@ export class OrdersComponent implements OnInit {
       status: 'Canceled',
     },
   ];
-  orderDetails: [
+  orderDetails= [
     {
-      sku: 'SHI-65483', product_name: 'Oculus VR', price: 999.29, qty: 1, disc: 5
+      id: 1, sku: 'SHI-65483', product_name: 'Oculus VR', price: 999.29, qty: 1, disc: 5
     },
     {
-      sku: 'SHI-65483', product_name: 'Note Diaries', price: 999.29, qty: 2, disc: 5
+      id: 2, sku: 'SHI-65483', product_name: 'Note Diaries', price: 999.29, qty: 2, disc: 5
     },
     {
-      sku: 'SHI-65483', product_name: 'Apple iPhone 13', price: 999.29, qty: 3, disc: 5
+      id: 3, sku: 'SHI-65483', product_name: 'Apple iPhone 13', price: 999.29, qty: 3, disc: 5
     },
   ];
   
