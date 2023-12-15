b5984096916c7cfa540a459181706e52a687e7b4
Folorunso Olorunfemi Emmanuel <femite58@gmail.com>
2023-12-16 00:48:54 +0100

    update

--- src/app/administrator/pages/general-settings/company-info/company-info.component.ts
+++ src/app/administrator/pages/general-settings/company-info/company-info.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-company-info',
   templateUrl: './company-info.component.html',
-  styleUrls: ['./company-info.component.scss']
+  styleUrls: ['./company-info.component.scss'],
 })
-export class CompanyInfoComponent {
+export class CompanyInfoComponent implements OnInit {
+  submitting = false;
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    owner: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+    facebook_url: new FormControl(''),
+    twitter_url: new FormControl(''),
+    instagram_url: new FormControl(''),
+    linkedin_url: new FormControl(''),
+    terms_and_condition: new FormControl(''),
+    privacy_policy: new FormControl(''),
+  });
 
+  f(n) {
+    return this.form.get(n);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {}
 }
