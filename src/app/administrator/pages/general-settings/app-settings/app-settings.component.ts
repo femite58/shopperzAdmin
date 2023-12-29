82d34f094419da63744513e7f26b8641cbc9d6bc
Folorunso Olorunfemi Emmanuel <femite58@gmail.com>
2023-12-29 16:03:44 +0100

    update

--- src/app/administrator/pages/general-settings/app-settings/app-settings.component.ts
+++ src/app/administrator/pages/general-settings/app-settings/app-settings.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-app-settings',
   templateUrl: './app-settings.component.html',
-  styleUrls: ['./app-settings.component.scss']
+  styleUrls: ['./app-settings.component.scss'],
 })
-export class AppSettingsComponent {
+export class AppSettingsComponent implements OnInit {
+  form = new FormGroup({
+    app_name: new FormControl('Shopperz', Validators.required),
+    app_icon: new FormControl(
+      'assets/images/app_icons/app_icon3.svg',
+      Validators.required
+    ),
+    splash_screens: new FormArray([
+      new FormGroup({
+        title: new FormControl('', Validators.required),
+        description: new FormControl('', Validators.required),
+        image: new FormControl('', Validators.required),
+      }),
+    ]),
+  });
 
+  sc(i, n) {
+    return this.f('splash_screens').at(i).get(n);
+  }
+
+  f(n): any {
+    return this.form.get(n);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {}
 }
