07ff1bea9a6963e5bcce5ffb53a31924280408b1
Folorunso Olorunfemi Emmanuel <femite58@gmail.com>
2024-01-28 20:05:01 +0100

    update

--- apps/admin/src/app/pages/product-details/product-details.component.ts
+++ apps/admin/src/app/pages/product-details/product-details.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'libs/shared/src/lib/services/api/user/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent implements OnInit {
+  product;
+  pid;
+  constructor(private prodS: ProductService, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.params.subscribe((p) => {
+      this.pid = p.id;
+      this.getProduct();
+    });
+  }
+
+  private getProduct() {
+    this.product = {
+      id: this.pid,
+      name: 'Xerox iphone 13 pro max',
+      category: 'Electronics'
+    };
+  }
+}
